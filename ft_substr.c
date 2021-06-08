31f561eb15e0b4cbfdb31d78a99969de0ee227cc
rafaellacerda1530 <lacerda.rafael1530@gmail.com>
2021-06-08 17:28:50 -0300

    fixing ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -15,11 +15,15 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*n_s;
+	size_t	i;
 
 	if (s == NULL)
 		return (NULL);
-	if ((unsigned int)ft_strlen(s) < start || (unsigned int)ft_strlen(s) < len)
+	if ((unsigned int)ft_strlen(s) < start)
 		return (ft_strdup(""));
+	i = ft_strlen(s + start);
+	if (i < len)
+		len = i;
 	n_s = (char *)malloc(sizeof(char) * (len + 1));
 	if (n_s == NULL)
 		return (NULL);
